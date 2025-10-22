b5c4014650ae84c4ea6ce0e98a32176eaf673934
RafaelhenriqueCandido <rafael.candido.azevedo@escola.pr.gov.br>
2025-10-22 08:39:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const avance = document. querySelectronrAll(" .btn-proximo")
+
+vanca. forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('ativo')
+        const proximoPasso = 'passo-' + this.getAtribute('data-proximo')
+
+        atual.classList.remove('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo')
+    })
+})
